a096e912ec851bb7597260a9eb966d8373c0119d
JohnnySegura <115127955+JohnnySegura@users.noreply.github.com>
2023-05-20 11:39:31 -0500

    Update Box.java
    se hizo los cambios

--- Clase 6/Laboratorio 6/Box.java
+++ Clase 6/Laboratorio 6/Box.java
@@ -1,22 +1,30 @@
-package com.mycompany.laboratorio6;
+package com.miempresa.laboratorio6;
 
-public class Box {    
-    public class Point{
+public class Box {
+    public static class Point {
         public int x, y;
-        public Point(int x, int y){
+
+        public Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
-    
+
     public Point[] coords;
 
-    public Box(int x, int y, int width, int height){
+    public Box(int x, int y, int width, int height) {
         coords = new Point[4];
-        
+
         coords[0] = new Point(x, y);
         coords[1] = new Point(x + width, y);
         coords[2] = new Point(x, y + height);
         coords[3] = new Point(x + width, y + height);
+    }
+
+    public static void main(String[] args) {
+        Box box = new Box(0, 0, 10, 10);
+        for (Point point : box.coords) {
+            System.out.println("x: " + point.x + ", y: " + point.y);
+        }
     }
 }
